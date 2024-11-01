16186277085b4e3a4fd0f593dcfb206c66f600dd
KIRANBADAKURAVA <kiranbadakurava@gmail.com>
2024-11-01 11:41:33 +0530

    Products fetched

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,19 @@
 import Product from "../Components/Product/Product";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
-    useEffect(()=>{
+        const [Products, setProducts]= useState([])
 
-    })
-
+   useEffect(()=>{
+        fetch('https://fakestoreapi.com/products?limit=5')
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log('Products succesfully fetched ', data)
+            setProducts(data)
+          }
+          )
+   },[])
     return (
         <div className="container flex flex-col space-y-2 mb-2 ">
             <div className="Image h-80 w-full bg-blue-700">
@@ -14,34 +21,14 @@ export default function Home() {
             </div>
             <div className="bg-black text-white w-auto">Category</div>
             <div className="Products flex   space-x-2 justify-between">
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
+                {
+                    Products.map(()=>{
+
+                    })
+                }
                 
             </div>
-            <div className="bg-black text-white w-auto">Category</div>
-            <div className="Products flex   space-x-2 justify-around">
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-            </div>
-            <div className="bg-black text-white w-auto">Category</div>
-            <div className="Products flex   space-x-2 justify-around">
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-            </div>
+            
 
         </div>
     );
